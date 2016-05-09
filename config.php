60bef88a0689f0c6dacb2ba0b18274fc9c1ae975
SynthSy <synthsy@gmail.com>
2016-05-09 12:15:49 -0700

    Config file needed
    Uses PSO2es notifications from a file (local or url)

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	//Set timezone to Japan for date calculations
+	date_default_timezone_set('Asia/Tokyo');
+	
+	//Specify source of ES notification text 
+	$lines = file(''); //can be local file or url
+	foreach($lines as $line)	
+	?>
